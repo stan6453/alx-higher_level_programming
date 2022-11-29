c732b8708cdb11fe04c1238a8988efb6b0faf41e
stanley <stanley6453@gmail.com>
2022-11-29 17:09:57 +0100

    fixed errors on file 100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -4,4 +4,4 @@ for number in reversed(range(97, 123)):
         pass
     else:
         number = number - 32
-    print("{number:c}".format(number), end="")
+    print("{:c}".format(number), end="")
