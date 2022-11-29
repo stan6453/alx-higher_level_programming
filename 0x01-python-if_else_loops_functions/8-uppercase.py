85c408be86cc5d175da96c83d9d8ee770cafc122
stanley <stanley6453@gmail.com>
2022-11-29 22:02:19 +0100

    fixed errors in 8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -4,5 +4,5 @@ def uppercase(str):
         number = ord(letter)
         if number >= 97 and number <= 122:
             number = number - 32
-        print("{:c}".format(letter), end="")
-    print()
+        print("{:c}".format(number), end="")
+    print("")
