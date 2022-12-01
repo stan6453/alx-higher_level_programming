f759951ebdcc5dc17eeedf4d2db173287ec08fd1
stanley <stanley6453@gmail.com>
2022-12-02 00:15:57 +0100

    edited file 4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -8,11 +8,12 @@ f.read(16)
 
 code = marshal.load(f)
 consts = []
+count = 1
 for item in code.co_consts:
-    not_in = itme not in ("__init__", None)
-    if type(item) != type(code.co_consts[0]) and not_in:
+    not_in = item not in ("__init__", None)
+    if count % 2 == 0 and not_in:
         consts.append(item)
-
+    count = count + 1
 consts.sort()
 for string in consts:
     if __name__ == '__main__':
