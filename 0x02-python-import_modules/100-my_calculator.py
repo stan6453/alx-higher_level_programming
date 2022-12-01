dc1db833519385dfb9808396ebd2639059764f60
stanley <stanley6453@gmail.com>
2022-12-01 15:28:54 +0100

    error fix in file 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -7,7 +7,6 @@ if len(sys.argv) != 4:
     print("Usage: {} <a> <operator> <b>".format(sys.argv[0]))
     sys.exit(1)
 
-
 operator = sys.argv[2]
 
 if operator not in ('+', '-', '*', '/'):
@@ -16,7 +15,6 @@ if operator not in ('+', '-', '*', '/'):
 
 a = int(sys.argv[1])
 b = int(sys.argv[3])
-
 
 if operator == '+':
     result = add(a, b)
