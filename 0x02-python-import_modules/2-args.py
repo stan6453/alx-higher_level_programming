b4aa33b297282544db8283279a40e2a360dd2619
stanley <stanley6453@gmail.com>
2022-12-01 15:07:10 +0100

    error fix in file 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,13 +4,13 @@ from sys import argv
 
 # argument count minus the name of the script
 argc = len(argv) - 1
-
-if argc < 1:
-    print("0 arguments.")
-else:
-    if argc == 1:
-        print(f"{argc} argument:")
+if __name__ == '__main__':
+    if argc < 1:
+        print("0 arguments.")
     else:
-        print(f"{argc} arguments:")
-    for index in range(1, argc + 1):
-        print(f"{index}: {argv[index]}")
+        if argc == 1:
+            print(f"{argc} argument:")
+        else:
+            print(f"{argc} arguments:")
+        for index in range(1, argc + 1):
+            print(f"{index}: {argv[index]}")
