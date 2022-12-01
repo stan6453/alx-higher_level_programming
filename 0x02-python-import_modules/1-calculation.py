49114f924f4ef63702a3ccda7119d365bf8aff00
stanley <stanley6453@gmail.com>
2022-12-01 15:35:29 +0100

    error fix in file 1-calculation.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -5,7 +5,7 @@ from calculator_1 import add, mul, div, sub
 a = 10
 b = 5
 if __name__ == '__main__':
-    print(f"{a} + {b} = {add(a, b)}")
-    print(f"{a} - {b} = {sub(a, b)}")
-    print(f"{a} * {b} = {mul(a, b)}")
-    print(f"{a} / {b} = {div(a, b)}")
+    print(f"{} + {} = {}".format(a, b, add(a, b)))
+    print(f"{} - {} = {}".format(a, b, sub(a, b)))
+    print(f"{} * {} = {}".format(a, b, mul(a, b)))
+    print(f"{} / {} = {}".format(a, b, div(a, b)))
