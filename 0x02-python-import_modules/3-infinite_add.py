d3b23ae9f36568f33d4cabd731d6bc518faca390
stanley <stanley6453@gmail.com>
2022-12-01 15:11:36 +0100

    error fix in file 3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,7 +3,7 @@ from sys import argv
 total = 0
 if len(argv) > 1:
     for index in range(1, len(argv)):
-       total += int(argv[index])
+        total += int(argv[index])
 
 if __name__ == '__main__':
     print(total)
