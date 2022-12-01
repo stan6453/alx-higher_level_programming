412b230a18c74a91646a8e10070869a5de3e2f4a
stanley <stanley6453@gmail.com>
2022-12-01 14:54:22 +0100

    error fix in file 0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-if __name__ == '__main__':
-    import sys
-    sys.exit()
+
 from add_0 import add
 a = 1
 b = 2
+if __name__ == '__main__':
+    import sys
+    sys.exit()
 print("{} + {} = {}".format(a, b, add(a, b)))
