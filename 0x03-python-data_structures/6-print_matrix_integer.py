356eb90198a063c06c1b1ff71d5172105a29187c
stanley <stanley6453@gmail.com>
2022-12-04 23:24:59 +0100

    modified file  6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if not matrix:
-        return
     for list1 in matrix:
-        if not list1:
-            continue
-        print("{:d} {:2d} {:2d}".format(list1[0],list1[1],list1[2]))
+        for element in list1:
+            if id(element) == id(list1[0]):
+                print("{:d}".format(element), end = "")
+            else:
+                print("{:2d}".format(element), end="")
+        print()    
