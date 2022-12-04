76ba2f8223d2e74c0851815b99baa9b9d7bf5720
stanley <stanley6453@gmail.com>
2022-12-04 23:11:03 +0100

    created file def 6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,5 @@
 def print_matrix_integer(matrix=[[]]):
     for list1 in matrix:
         for element in list1:
-            print("{:2d}".format(element),end="")
+            print("{:2d}".format(element), end="")
         print()
