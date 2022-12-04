2d5a5e5f90c20d68c229abd4415695a7aa1969e9
stanley <stanley6453@gmail.com>
2022-12-04 23:03:03 +0100

    created file def 5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,7 +2,7 @@
 def no_c(my_string):
     if not my_string:
         return
-    new_str =""
+    new_str = ""
     for letter in my_string:
         if letter != 'C' and letter != 'c':
             new_str = new_str + letter
