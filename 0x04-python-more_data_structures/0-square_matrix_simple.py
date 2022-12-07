f8607fb631b1c545aed59f8cf23189cd52c45abc
stanley <stanley6453@gmail.com>
2022-12-07 08:08:16 +0100

    created file 0-square_matrix_simple.py

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+
+def square_matrix_simple(matrix=[]):
+    return [[item**2 for item in list1] for list1 in matrix]
---
+++ 0x04-python-more_data_structures/README.md
@@ -0,0 +1,1 @@
+# This directory contaions my solutions to the alx project 0x04. Python - More Data Structures: Set, Dictionary
