13e5f69a4273705450abfd2c144db774dbc6b5a8
stanley <stanley6453@gmail.com>
2022-12-07 19:34:56 +0100

    special commit from root folder

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,3 +1,16 @@
 #!/usr/bin/python3
-def roman_to_int(roman_string):
 
+numbers = {
+        'I':1,
+        'V':5,
+        'X':10,
+        'L':50,
+        'C':100,
+        'D':500,
+        'M':1000,
+        }
+seq = ['I','V','X','L','C','D','M']
+def roman_to_int(roman_string):
+    for index, letter in enumerate(string):
+        if numbers[letter] in (1, 10, 100, 1000):
+            if string[index+1] == seq.index()
