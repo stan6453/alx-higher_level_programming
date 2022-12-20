36f16b074a6a9e186010776839c3cad7ce156901
stanley <stanley6453@gmail.com>
2022-12-20 16:29:09 +0100

    first commit for task 0x06. Python - Classes and Objects

---
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+
+class Node:
+    def __init__(self, data = 0, next_node=None):
+        if type(data) is not int:
+            raise TypeError("data must be an integer")
+        if type(next_node) is not Node and next_node is not None:
+            raise TypeError("next_node must be a Node object")
+        self.__data = data
+        self.__next_node = next_node
+
+    @property
+    def data(self):
+        return self.__data
+
+    @data.setter
+    def data(self, value):
+        if type(value) is not int:
+            raise TypeError("data must be an integer")
+        self.__data = value
+
+    @property
+    def next_node(self):
+        return self.__next_node
+
+    @next_node.setter
+    def next_node(self, value):
+        if type(value) is not Node and value is not None:
+            raise TypeError("next_node must be a Node object")
+        self.__next_node = value
+
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.__head = None
+
+    def sorted_insert(self, value):
+        new_node = Node(value)
+
+        if self.__head is None:
+            self.__head = new_node
+        else:
+            temp = self.__head
+
+            while(temp.data < value and temp.next_node is not None and temp.next_node.data < value):
+                temp = temp.next_node
+            
+            if temp == self.__head and value < temp.data:
+                new_node.next_node = temp
+                self.__head = new_node
+                return
+
+            new_node.next_node = temp.next_node
+            temp.next_node = new_node
+
+    def __str__(self):
+        output =""
+        temp = self.__head
+
+        while(temp is not None):
+            output += str(temp.data)
+            output += "\n"
+            temp = temp.next_node
+
+        return output
---
+++ 0x06-python-classes/README.md
@@ -0,0 +1,1 @@
+# This directory contains my solutions to ALX project on 0x06. Python - Classes and Objects
