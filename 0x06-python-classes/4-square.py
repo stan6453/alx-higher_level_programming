06fa79bb7df096a21b385d0991842bb207c0fb0e
stanley <stanley6453@gmail.com>
2022-12-20 17:09:52 +0100

    fixing Python Docstrings

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
+"""
+Testing Python Docstrings
+"""
+
+
 class Square:
+     """
+    class has a size private instance property
+    """
+
     def __init__(self, size=0):
         if type(size) is not int:
             raise TypeError("size must be an integer")
