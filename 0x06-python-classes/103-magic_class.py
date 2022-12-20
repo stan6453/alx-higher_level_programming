336abfcdbce84504e1c240420f2e2c7399f7c8c6
stanley <stanley6453@gmail.com>
2022-12-20 19:38:17 +0100

    modified 103-magic_class.py

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,14 +1,10 @@
 #!/usr/bin/python3
-"""
-Testing Python Docstrings
-"""
 import math
 
 
 class MagicClass:
-    def __init__(self, radius):
-        self.__radius = 0
-        if type(radius) is not int and  type(radius) is not float:
+    def __init__(self, radius=0):
+        if type(radius) is not int and type(radius) is not float:
             raise TypeError("radius must be a number")
         else:
             self.__radius = radius
