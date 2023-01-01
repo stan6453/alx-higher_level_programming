0cef410160314261cf3e326996b2b93f31ee2c75
stanley <stanley6453@gmail.com>
2023-01-01 16:22:49 +0100

    added file tests/6-max_integer_test.py

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,2 +1,27 @@
 #!/usr/bin/python3
-"""T"""
+"""Text indent module."""
+
+
+def text_indentation(text):
+    """
+    Print a text with 2 new lines after each of thesecharacters.
+
+    '.', '?' and ':'.
+    """
+    """Tests"""
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+
+    """Operation Logic."""
+    new_text = ""
+    new_line = False
+    for character in text:
+        if character in [".", "?", ":"]:
+            print(character + "\n\n", end="")
+            new_line = True
+        else:
+            if new_line and character == " ":
+                new_line = False
+                continue
+            new_line = False
+            print(character, end="")
