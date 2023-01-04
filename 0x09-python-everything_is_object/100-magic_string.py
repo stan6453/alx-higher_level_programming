0c9415adabf29a5eecf58417e4d592376b77076b
stanley <stanley6453@gmail.com>
2023-01-04 21:04:36 +0100

    modified file /0x09-python-everything_is_object/100-magic_string.py

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -4,5 +4,7 @@
 
 def magic_string():
     """Magic function"""
-     magic_string.counter += 1; return ("BestSchool, " * (magic_string.counter))[:-2]
+    magic_string.counter += 1; return ("BestSchool, " * magic_string.counter)[:-2]
+
+
 magic_string.counter = 0
