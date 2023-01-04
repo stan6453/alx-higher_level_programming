4d4147098996db9c8c4095ef4cbb9ef736b476db
stanley <stanley6453@gmail.com>
2023-01-04 04:20:11 +0100

    created file 101-locked_class.py

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -2,6 +2,7 @@
 class LockedClass:
     def __setattr__(self, name, value):
         if name != 'first_name':
-            raise AttributeError("You are not allowed to set new instance attributes")
+            raise AttributeError("You are not allowed to \
+                    set new instance attributes")
         else:
             self.__dict__[name] = value
