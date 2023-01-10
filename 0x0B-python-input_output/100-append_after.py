93d1f35e6f14537ddfdceda2baf833702530f32a
stanley <stanley6453@gmail.com>
2023-01-10 22:57:29 +0100

    modified file 100-append_after.py

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -3,5 +3,13 @@
 
 
 def append_after(filename="", search_string="", new_string=""):
+    """inserts a line of text (new_string) to a file,
+    after each line containing a specific string (search_string)"""
+    buf = ""
     with open(filename) as fread:
-        with open(filename, "a") as fwrite:
+        for line in fread:
+            buf += line
+            if search_string in line:
+                buf += new_string
+    with open(filename, "w") as fwrite:
+        fwrite.write(buf)
