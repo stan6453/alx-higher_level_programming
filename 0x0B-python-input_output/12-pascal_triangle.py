b4d5fb1a51801d28157592710e72ba8e71de9ceb
stanley <stanley6453@gmail.com>
2023-01-10 22:29:06 +0100

    modified file 12-pascal_triangle.py

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -13,15 +13,18 @@ def pascal_triangle(n):
         if len(outer_list) == 0:
             outer_list.append([1])
             continue
+        if len(outer_list) == 1:
+            outer_list.append([1, 1])
+            continue
         prev_list = outer_list[len(outer_list) - 1]
         next_list = []
         first_item = 1
-        for num, index in enumerate(prev_list):
+        for index, num in enumerate(prev_list):
             if first_item:
                 next_list.append(num)
                 first_item = 0
             else:
-                if prev_list[index + 1]:
+                if index + 1 != len(prev_list):
                     next_list.append(prev_list[index - 1] + prev_list[index])
                 else:
                     next_list.append(prev_list[index - 1] + prev_list[index])
