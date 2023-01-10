c11df74c0d28421af66fc68f7286fdd12c42d1e2
stanley <stanley6453@gmail.com>
2023-01-11 00:10:45 +0100

    modified file 101-stats.py

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -8,20 +8,26 @@ while True:
                     403: 0, 404: 0, 405: 0, 500: 0}
     total_size = 0
     line_count = 0
-    line = input()
-    while line:
-        file_size = int(line[line.rfind(" ") + 1:])
-        status_code = int(line[line.rfind(" ", 0,
-                                          line.rfind(" ") - 1) +
-                               1:line.rfind(" ")])
-        total_size += file_size
-        status_codes[status_code] += 1
-        line_count += 1
-        if line_count == 10 * iteration:
-            print("File size:", total_size)
-            for key, value in status_codes.items():
-                if value:
-                    print("{}:".format(key), value)
-            break
-        new_line = input()
-    iteration += 1
+    try:
+        line = input()
+        while line:
+            file_size = int(line[line.rfind(" ") + 1:])
+            status_code = int(line[line.rfind(" ", 0,
+                                              line.rfind(" ") - 1) +
+                                   1:line.rfind(" ")])
+            total_size += file_size
+            status_codes[status_code] += 1
+            line_count += 1
+            if line_count == 10 * iteration:
+                print("File size:", total_size)
+                for key, value in status_codes.items():
+                    if value:
+                        print("{}:".format(key), value)
+                break
+            new_line = input()
+        iteration += 1
+    except KeyboardInterrupt:
+        print("File size:", total_size)
+        for key, value in status_codes.items():
+            if value:
+                print("{}:".format(key), value)
