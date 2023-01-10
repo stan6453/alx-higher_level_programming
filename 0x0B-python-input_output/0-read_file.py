73c33486316f1fb8a2a2c268b3952a29da66e1d9
stanley <stanley6453@gmail.com>
2023-01-10 15:48:02 +0100

    modified file 0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,4 +5,4 @@
 def read_file(filename=""):
     """print content of a file to stdout"""
     with open(filename, "rt", encoding="utf-8") as f:
-        print(f.read())
+        print(f.read(), end="")
