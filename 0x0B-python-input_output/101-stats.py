9092a32d748c5c1e22ad01053d05400eb3b2a2fa
stanley <stanley6453@gmail.com>
2023-01-11 00:02:39 +0100

    modified file 101-stats.py

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -2,6 +2,7 @@
 """Web Request Stats module"""
 
 
+iteration = 1
 while True:
     status_codes = {200: 0, 301: 0, 400: 0, 401: 0,
                     403: 0, 404: 0, 405: 0, 500: 0}
@@ -16,10 +17,11 @@ while True:
         total_size += file_size
         status_codes[status_code] += 1
         line_count += 1
-        if line_count == 10:
+        if line_count == 10 * iteration:
             print("File size:", total_size)
             for key, value in status_codes.items():
                 if value:
                     print("{}:".format(key), value)
             break
         new_line = input()
+    iteration += 1
