f53cf0d523f6b52f84f065407d1ac3c2e32c533c
stanley <stanley6453@gmail.com>
2023-01-10 23:51:23 +0100

    added file101-stats.py

---
+++ 0x0B-python-input_output/101-stats.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+
+while True:
+    status_codes = {200: 0, 301: 0, 400: 0, 401: 0,
+                    403: 0, 404: 0, 405: 0, 500: 0}
+    total_size = 0
+    line_count = 0
+    line = input()
+    while line:
+        file_size = int(line[line.rfind(" ") + 1:])
+        status_code = int(line[line.rfind(" ", 0,
+line.rfind(" ") - 1) + 1:line.rfind(" ")])
+        total_size += file_size
+        status_codes[status_code] += 1
+        line_count += 1
+        if line_count == 10:
+            print("File size:", total_size)
+            for key, value in status_codes.items():
+                if value:
+                    print("{}:".format(key), value)
+            break
+        new_line = input()
