3956d7a0e9f2416ceee2693aac0283c67c7cc9fb
stanley <stanley6453@gmail.com>
2023-01-10 16:24:37 +0100

    modified file 11-student.py

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -5,14 +5,14 @@
 class Student:
     """Student class"""
     def __init__(self, first_name, last_name, age):
-    	"""Initializes a new Student"""
+        """Initializes a new Student"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-    	"""Return a dictionary representaion of the object based on the keys
-    	contained in attrs"""
+        """Return a dictionary representaion of the object based on the keys
+        contained in attrs"""
         if attrs is None:
             return self.__dict__
         new_dict = {}
@@ -22,5 +22,5 @@ class Student:
         return new_dict
 
     def reload_from_json(self, json):
-    	"""Replaces all attributes of the Student"""
+        """Replaces all attributes of the Student"""
         self.__dict__ = json
