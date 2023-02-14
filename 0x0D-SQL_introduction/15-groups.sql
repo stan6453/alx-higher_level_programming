a91cda3e1b7ddbcbb468e02ba2f9383f5884f7be
stanley <stanley6453@gmail.com>
2023-02-14 16:05:35 +0100

    error fix

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,5 +1,7 @@
--- lists the number of records with the same score in
--- the table second_table of the database hbtn_0c_0
+/*
+    lists the number of records with the same score in
+    the table second_table of the database hbtn_0c_0
+*/
 
 SELECT score, COUNT(DISTINCT score) AS number 
 FROM second_table
