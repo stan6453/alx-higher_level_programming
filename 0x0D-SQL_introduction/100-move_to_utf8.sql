2da0b15c77577b125194d9af3868c9f61ee086fe
stanley <stanley6453@gmail.com>
2023-02-14 19:21:33 +0100

    error fix

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -2,5 +2,4 @@
 -- (utf8mb4, collate utf8mb4_unicode_ci) 
 
 USE `hbtn_0c_0`;
-ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
