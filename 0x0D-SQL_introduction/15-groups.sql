524224cc5d0de2326bb2f542b666559252ad031b
stanley <stanley6453@gmail.com>
2023-02-14 15:04:37 +0100

    minor changes

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,7 +1,7 @@
 -- lists the number of records with the same score in
 -- the table second_table of the database hbtn_0c_0
 
-SELECT DISTINCT score, COUNT(DISTINCT score) AS number 
+SELECT score, COUNT(DISTINCT score) AS number 
 FROM second_table
 GROUP BY score
 ORDER BY number DESC;
