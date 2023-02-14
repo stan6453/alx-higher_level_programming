52c0a86e909448a1662387fe44af21f05397aa26
stanley <stanley6453@gmail.com>
2023-02-14 16:12:05 +0100

    error fix

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,6 +1,6 @@
 -- lists the number of records with the same score in
 
-SELECT score, COUNT(DISTINCT score) AS number 
+SELECT score, COUNT(score) AS number 
 FROM second_table
 GROUP BY score
 ORDER BY number DESC;
