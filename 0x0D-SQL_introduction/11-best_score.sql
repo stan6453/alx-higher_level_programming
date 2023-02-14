19d4e6c9a044aae64429abd2aa18924db32d9b39
stanley <stanley6453@gmail.com>
2023-02-14 14:53:40 +0100

    minor changes

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,4 +1,4 @@
---lists all records with a score >= 10 in the table second_table of the database hbtn_0c_0
+-- lists all records with a score >= 10 in the table second_table of the database hbtn_0c_0
 
-SELECT * FROM hbtn_0c_0.second_table
-WHERE score >= 10
+SELECT * FROM second_table
+WHERE score >= 10;
