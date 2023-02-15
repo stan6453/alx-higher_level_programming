6a2e2e041968f3996a8a83fd699aa7fac8be3add
stanley <stanley6453@gmail.com>
2023-02-15 17:58:15 +0100

    updated the directory /0x0E-SQL_more_queries with some more files

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,5 +1,6 @@
---  lists all shows without the genre Comedy in the database hbtn_0d_tvshows
+-- lists all shows without the genre Comedy in the database hbtn_0d_tvshows
 SELECT DISTINCT tv_shows.title FROM tv_shows LEFT JOIN tv_show_genres
 ON tv_show_genres.show_id = tv_shows.id
-WHERE tv_show_genres.genre_id != (SELECT id FROM tv_genres WHERE tv_genres.name="Comedy")
+WHERE tv_show_genres.genre_id != (SELECT id FROM tv_genres WHERE tv_genres.name="Comedy") 
+OR tv_show_genres.genre_id IS NULL 
 ORDER BY tv_shows.title;
