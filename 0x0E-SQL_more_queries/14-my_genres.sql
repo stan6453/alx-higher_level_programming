c95bfee24c995cf76bcbac768337532ff3071419
stanley <stanley6453@gmail.com>
2023-02-15 16:50:50 +0100

    updated the directory /0x0E-SQL_more_queries with some more files

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,5 +1,5 @@
 -- lists all genres of the show Dexter
 SELECT tv_genres.name FROM tv_genres RIGHT JOIN tv_show_genres
 ON tv_show_genres.genre_id = tv_genres.id RIGHT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
-WHERE title = "Dexter"
+WHERE tv_shows.title = "Dexter"
 GROUP BY tv_genres.name;
