b986756155a33dc438cb69bc28f96b3cd7f614b9
stanley <stanley6453@gmail.com>
2023-02-15 16:57:13 +0100

    updated the directory /0x0E-SQL_more_queries with some more files

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -2,4 +2,4 @@
 SELECT DISTINCT tv_genres.name FROM tv_genres RIGHT JOIN tv_show_genres
 ON tv_show_genres.genre_id = tv_genres.id LEFT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
 WHERE tv_shows.title = "Dexter"
-GROUP BY tv_genres.name;
+ORDER BY tv_genres.name;
