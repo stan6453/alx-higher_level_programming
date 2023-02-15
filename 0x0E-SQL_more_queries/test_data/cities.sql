8961435c0d0d2420f070b415c5c2df692f864c37
stanley <stanley6453@gmail.com>
2023-02-15 12:12:45 +0100

    updated the directory /0x0E-SQL_more_queries with some more files

---
+++ 0x0E-SQL_more_queries/test_data/cities.sql
@@ -0,0 +1,6 @@
+INSERT INTO cities(id,state_id,name) VALUES (1,1,'San Francisco');
+INSERT INTO cities(id,state_id,name) VALUES (2,1,'San Jose');
+INSERT INTO cities(id,state_id,name) VALUES (4,2,'Page');
+INSERT INTO cities(id,state_id,name) VALUES (6,3,'Paris');
+INSERT INTO cities(id,state_id,name) VALUES (7,3,'Houston');
+INSERT INTO cities(id,state_id,name) VALUES (8,3,'Dallas');
---
+++ 0x0E-SQL_more_queries/test_data/states.sql
@@ -0,0 +1,4 @@
+INSERT INTO states(id,name) VALUES (1,'California');
+INSERT INTO states(id,name) VALUES (2,'Arizona');
+INSERT INTO states(id,name) VALUES (3,'Texas');
+INSERT INTO states(id,name) VALUES (4,'Utah');
