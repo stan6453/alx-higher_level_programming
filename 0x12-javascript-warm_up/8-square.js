c1bea59ea11f832074cbfeb312c79c655cedbe69
stanley <stanley6453@gmail.com>
2023-03-13 15:14:40 +0100

    added file 0x12-javascript-warm_up/8-square.js

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -4,7 +4,7 @@ if (!isNaN(num)) {
   num = Number(num);
   for (let i = 0; i < num; i++) {
     for (let j = 0; j < num; j++) {
-      process.stdout.write('x');
+      process.stdout.write('X');
     }
     console.log('');
   }
