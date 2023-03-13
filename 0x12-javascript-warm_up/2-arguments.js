e6fc27438e95083fc347f10015d81455c4c98707
stanley <stanley6453@gmail.com>
2023-03-13 14:46:38 +0100

    error fix

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
 const argCount = process.argv.length;
 argCount <= 2 && console.log('No argument');
-argCount > 2 && console.log('Argument found');
+argCount === 3 && console.log('Argument found');
+argCount > 3 && console.log('Arguments found');
