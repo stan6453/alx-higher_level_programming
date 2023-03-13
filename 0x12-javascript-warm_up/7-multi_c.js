fcf90a5718bf718f8e710610778fec58cfdf29c4
stanley <stanley6453@gmail.com>
2023-03-13 15:02:06 +0100

    added file 0x12-javascript-warm_up/7-multi_c.js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,16 +1,11 @@
 #!/usr/bin/node
 const num = process.argv[2];
-if (num)
-{
-	if (!isNaN(num))
-	{
-		for (let i = 0; i < Number(num); i++)
-		{
-			console.log('C is fun');
-		}
-	}
-	else
-	{
-		console.log('Missing number of occurrences');
-	}
+if (num) {
+  if (!isNaN(num)) {
+    for (let i = 0; i < Number(num); i++) {
+      console.log('C is fun');
+    }
+  } else {
+    console.log('Missing number of occurrences');
+  }
 }
