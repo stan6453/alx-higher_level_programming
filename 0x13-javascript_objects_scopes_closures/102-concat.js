c67702a59ab50a77b076b4b34d2fb6c6acf33cd7
stanley <stanley6453@gmail.com>
2023-03-14 15:36:56 +0100

    added file 0x13-javascript_objects_scopes_closures/102-concat.js

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -2,7 +2,7 @@
 const fs = require('fs');
 
 for (const args of process.argv.slice(2)) {
-  fs.readFile(args, function (err, data) {
+  fs.readFile(args, (err, data) => {
     if (!err) {
       process.stdout.write(data.toString());
     }
