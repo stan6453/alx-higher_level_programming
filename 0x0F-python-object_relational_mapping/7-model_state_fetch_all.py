593a11747627ee9e656dc9c46d581fa876a192a1
stanley <stanley6453@gmail.com>
2023-03-17 16:09:57 +0100

    added file 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
         'mysql+mysqldb://{}:{}@localhost/{}'
         .format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
 
-    Session = sessionmaker()
+    Session = sessionmaker(bind=engine)
 
     session = Session()
 
