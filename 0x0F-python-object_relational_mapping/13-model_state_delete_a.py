5d4661bd43affb99c1efae6bb32a9a4e227c9cf0
stanley <stanley6453@gmail.com>
2023-03-17 17:23:26 +0100

    added file 0x0F-python-object_relational_mapping/13-model_state_delete_a.py

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+adds the State object “Louisiana”
+to the database hbtn_0e_6_usa
+"""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import State
+
+if __name__ == "__main__":
+    '''
+    user_name = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    '''
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'
+        .format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    users_to_delete = session.query(State).filter(State.name.like("%a%")).all()
+
+    if users_to_delete:
+        for user in users_to_delete:
+            session.delete(user)
+        session.commit()
