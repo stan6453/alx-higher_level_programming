2b93bebb48cd3f0fdf7ae449745298b7ec0178e5
stanley <stanley6453@gmail.com>
2023-03-17 07:59:16 +0100

    created file 0x0F-python-object_relational_mapping/5-filter_cities.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     db_name = sys.argv[3]
 
     conn = MySQLdb.connect(host="localhost", port=3906, user=user_name,
-                        passwd=password, db=db_name, charset="utf8")
+                           passwd=password, db=db_name, charset="utf8")
     cur = conn.cursor()
     cur.execute("SELECT * FROM states ORDER BY id ASC")
     query_rows = cur.fetchall()
