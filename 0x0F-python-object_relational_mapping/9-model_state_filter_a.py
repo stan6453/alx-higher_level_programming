907e01a0ecf8f242978ca0f894a2f97e418711bc
stanley <stanley6453@gmail.com>
2023-03-17 16:43:28 +0100

    added file 0x0F-python-object_relational_mapping/9-model_state_filter_a.py

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -21,8 +21,7 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    states = session.query(State).filter(State.name.like('%a%'))\
-        .order_by(State.id).all()
+    states = session.query(State).order_by(State.id)
 
     for state in states:
         if "a" in state.name:
