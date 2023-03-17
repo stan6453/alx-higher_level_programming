c86593047219175b78de5205dc1f16dd25a9b939
stanley <stanley6453@gmail.com>
2023-03-17 08:20:19 +0100

    bug fix on file 2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     cur = conn.cursor()
     cur.execute("SELECT * FROM states\
                 WHERE name=%s\
-                ORDER BY id ASC", (state, ))
+                ORDER BY id ASC", state)
     query_rows = cur.fetchall()
     for row in query_rows:
         if state[1] == sys.argv[4]:
