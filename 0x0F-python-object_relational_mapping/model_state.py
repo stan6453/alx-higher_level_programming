cac7ca55c1a03cb69e48e9664141c2bcbeb8ef95
stanley <stanley6453@gmail.com>
2023-03-17 15:27:17 +0100

    created file 0x0F-python-object_relational_mapping/model_state.py

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+State Model
+"""
+
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class State(Base):
+    """
+    A class representation of the State table of mysql database
+    """
+    id = Column("id", Integer, primary_key=True)
+    name = Column("name", String(128), nullable=False)
