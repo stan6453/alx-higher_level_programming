7421f3950a1d1af70057ff02c4e0ed0222bc1626
stanley <stanley6453@gmail.com>
2023-03-17 16:56:41 +0100

    added file 0x0F-python-object_relational_mapping/10-model_state_my_get.py

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 """
- prints the State object with the name passed
- as argument from the database hbtn_0e_6_usa
+Lists the State object with the name passed as argument
+from the database hbtn_0e_6_usa.
+Usage: ./10-model_state_my_get.py <mysql username> /
+                                  <mysql password> /
+                                  <database name>
+                                  <state name searched>
 """
 import sys
 from sqlalchemy import create_engine
@@ -9,23 +13,17 @@ from sqlalchemy.orm import sessionmaker
 from model_state import State
 
 if __name__ == "__main__":
-    '''
-    user_name = sys.argv[1]
-    password = sys.argv[2]
-    db_name = sys.argv[3]
-    '''
-    engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost/{}'
-        .format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    name = sys.argv[3]
-
-    state = session.query(State).filter(State.name == name).first()
-
-    if state:
-        print(f"{state.id}")
-    else:
+    found = False
+    for state in session.query(State):
+        if state.name == sys.argv[4]:
+            print("{}".format(state.id))
+            found = True
+            break
+    if found is False:
         print("Not found")
