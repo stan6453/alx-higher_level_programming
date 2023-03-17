1dc384af2de5812ec7b7b621ca27592c4d34697a
stanley <stanley6453@gmail.com>
2023-03-17 16:01:23 +0100

    added file 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -2,26 +2,26 @@
 """
 Query the DB for states record using SQLAlchemy
 """
-
+import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
+from model_state import State
 
-'''
-user_name = sys.argv[1]
-password = sys.argv[2]
-db_name = sys.argv[3]
-'''
-engine = create_engine(
-    'mysql+mysqldb://{}:{}@localhost/{}'
-    .format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-Base.metadata.create_all(engine)
+if __name__ == "__main__":
+    '''
+    user_name = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    '''
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'
+        .format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
 
-Session = sessionmaker()
+    Session = sessionmaker()
 
-session = Session()
+    session = Session()
 
-states = session.query(State).order_by(State.id).all()
+    states = session.query(State).order_by(State.id).all()
 
-for index, state in enumerate(states):
-    print(f"{index}: {state}")
+    for index, state in enumerate(states):
+        print(f"{index}: {state}")
