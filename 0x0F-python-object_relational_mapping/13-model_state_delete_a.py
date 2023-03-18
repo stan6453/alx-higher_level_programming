0ebe4aeda4e42d4953425213e9d8ec2499e65e75
stanley <stanley6453@gmail.com>
2023-03-18 11:33:37 +0100

    minor bug fix

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -25,6 +25,4 @@ if __name__ == "__main__":
     users_to_delete = session.query(State).filter(State.name.like("%a%")).all()
 
     if users_to_delete:
-        for user in users_to_delete:
-            session.delete(user)
-        session.commit()
+        session.delete(users_to_delete)
