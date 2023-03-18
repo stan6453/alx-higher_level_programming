20575b5023a9addf390638c1f9596fde561bddfc
stanley <stanley6453@gmail.com>
2023-03-18 16:15:56 +0100

    added file 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py

---
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+prints all City objects from the database hbtn_0e_14_usa
+"""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from relationship_state import State
+from relationship_city import City
+
+if __name__ == "__main__":
+    '''
+    user_name = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    '''
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'
+        .format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    result = session.query(State, City).filter(State.id == City.state_id)\
+        .order_by(State.id).order_by(City.id).all()
+
+    processed = []
+    for state, city in result:
+        if state.name not in processed:
+            processed.append(state.name)
+            print(f'{state.id}: {state.name}')
+        print(f'\t{city.id}: {city.name}')
