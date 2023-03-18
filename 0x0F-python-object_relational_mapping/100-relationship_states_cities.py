c5b2592dbd6dd2d879b9628310585089e97e672b
stanley <stanley6453@gmail.com>
2023-03-18 12:05:25 +0100

    added files 0x0F-python-object_relational_mapping/{relationship_city.py, relationship_state.py, 100-relationship_states_cities.py}

---
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+prints all City objects from the database hbtn_0e_14_usa
+"""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import State
+from model_city import City
+
+if __name__ == "__main__":
+    '''
+    user_name = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    '''
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'
+        .format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = State(name='State')
+    state.cities = [City(name='San Francisco')]
+
+    session.add(state)
+    session.commit()
---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+SQLAlchemy State Model
+"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """
+    A class representation of the State table of mysql database
+    """
+    __tablename__ = "states"
+    id = Column("id", Integer, primary_key=True)
+    name = Column("name", String(128), nullable=False)
+
+    cities = relationship('City', backref='state')
