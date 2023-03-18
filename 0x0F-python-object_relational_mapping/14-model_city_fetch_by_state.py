0816018a16218af38093227686154d8310da05ee
stanley <stanley6453@gmail.com>
2023-03-18 11:46:51 +0100

    minor bug fix

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -25,6 +25,6 @@ if __name__ == "__main__":
     result = session.query(City, State)\
         .filter(State.id == City.state_id).order_by(City.id).all()
 
-    if result:
-        for city, state in result:
-            print(f"{state.name}: ({city.id}) {city.name} ")
+    
+    for city, state in result:
+        print(f"{state.name}: ({city.id}) {city.name} ")
