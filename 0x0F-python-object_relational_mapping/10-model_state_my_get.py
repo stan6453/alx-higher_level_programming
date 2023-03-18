a4aed24877c92d8461edf9a5ba1a478e4b0e3614
stanley <stanley6453@gmail.com>
2023-03-18 11:26:20 +0100

    error fix on file 0x0F-python-object_relational_mapping/10-model_state_my_get.py

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -21,7 +21,7 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    name = sys.argv[3]
+    name = sys.argv[4]
 
     state = session.query(State).filter(State.name == name).first()
 
