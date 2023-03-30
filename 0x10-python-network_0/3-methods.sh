3bebbfd4b0f37953a9c087c6455a707e215815e8
stanley <stanley6453@gmail.com>
2023-03-30 13:29:10 +0100

    creted file  0x10-python-network_0/3-methods.sh

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #takes in a URL and displays all HTTP methods the server will accept.
-curl -X "OPTIONS" "$1" -sLI | grep 'Allow' | cut -d":" -f2 | sed 's/^ //' 
+curl -X "OPTIONS" "$1" -sLI | grep 'Allow' | cut -d":" -f2 | sed 's/^ //'
