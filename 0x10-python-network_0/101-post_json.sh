e40f4b9f0e768bf0aa4b4d6e4d213451acf779d1
stanley <stanley6453@gmail.com>
2023-03-30 14:12:08 +0100

    creted file  0x10-python-network_0/101-post_json.sh

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #sends a JSON POST request to a URL passed as the first argument, and displays the body of the response.
-curl -v -L -d @"${2}" "$1" -H 'Content-Type: application/json'
+curl -L -d @"${2}" "$1" -H 'Content-Type: application/json'
