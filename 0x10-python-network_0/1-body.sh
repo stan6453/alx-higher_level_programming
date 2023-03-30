78e782241d211f8d07721b54129572e7a2257e6d
stanley <stanley6453@gmail.com>
2023-03-30 12:51:22 +0100

    creted file  0x10-python-network_0/1-body.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a GET request to the URL, and displays the body of the response
-curl -si "$1" | awk 'NR == 1 && $2 == 301 {show_body = 1} length($0) == 1 {body = 1; next} show_body == 1 && body == 1 {print $0}'
+curl -si "$1" | awk 'NR == 1 && $2 == 200 {show_body = 1} length($0) == 1 {body = 1; next} show_body == 1 && body == 1 {print $0}'
