9165bb1b01a4cc23abc0a64a159bf43cddcc3ca0
stanley <stanley6453@gmail.com>
2023-03-30 12:19:19 +0100

    creted file  0x10-python-network_0/0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # takes in a URL, sends a request to that URL, and displays the size of the body of the response
 
-url=$1
-curl -sI $url | grep 'Content-Length' | cut -d' ' -f2
+url="$1"
+curl -sI "$url" | grep 'Content-Length' | cut -d' ' -f2
