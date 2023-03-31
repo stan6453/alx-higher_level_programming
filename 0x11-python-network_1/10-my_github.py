c2b0f37206d50772062492aba7c77963530855b5
stanley <stanley6453@gmail.com>
2023-03-31 15:07:18 +0100

    error fix

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """
-takes your GitHub credentials (username and password) and uses the GitHub API to display your id
+takes your GitHub credentials (username and password)
+and uses the GitHub API to display your id
 """
 
 import requests
 from sys import argv
 if __name__ == "__main__":
     with requests.post('https://api.github.com/user',
-                  headers={'Authorization': f"Bearer {argv[2]}"}) as res:
+                       headers={'Authorization': f"Bearer {argv[2]}"}) as res:
         print(res.json()['id'])
