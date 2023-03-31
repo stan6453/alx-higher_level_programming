4dab51e0461950541b71280b3aa32852451d7c32
stanley <stanley6453@gmail.com>
2023-03-31 04:39:28 +0100

    creted file  0x10-python-network_0/102-catch_me.sh

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Bash script that makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!, in the body of the response.
-curl -vL -k -X "PUT" -d "user_id=98&school=School&coming_from=School" -H "Referer: School" -A "School" -c "./cookie/catch_me_cookies" -b "./cookie/catch_me_cookies" "$1"
+curl -sL -k -X "PUT" -d "user_id=98&school=School&coming_from=School" -H "Referer: School" -A "School" -c "./cookie/catch_me_cookies" -b "./cookie/catch_me_cookies" "$1"
