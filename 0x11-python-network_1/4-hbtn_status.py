2688618faeceb4a648d01a00208197a763455db9
stanley <stanley6453@gmail.com>
2023-03-31 14:04:00 +0100

    error fix

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """fetch https://alx-intranet.hbtn.io/status"""
 
-import urllib.request
+import requests
 if __name__ == "__main__":
-    with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as res:
+    with requests.get("https://alx-intranet.hbtn.io/status") as res:
         print('Body response:')
-        print(f"\t- type: {type(res.read().decode('utf-8'))}")
-        print(f"\t- content: {res.read().decode('utf-8')}")
+        print(f"\t- type: {type(res.text)}")
+        print(f"\t- content: {res.text}")
