ce76e56ecee426affdd495e7f4000b6671562760
stanley <stanley6453@gmail.com>
2023-03-31 14:13:07 +0100

    error fix

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -5,16 +5,12 @@ sends a POST request to the passed URL with the email as a parameter,
 and displays the body of the response (decoded in utf-8)
 """
 
-import urllib.parse
-import urllib.request
+import requests
 from sys import argv
 
 if __name__ == "__main__":
     url = argv[1]
-    value = {'email': argv[2]}
+    values = {'email': argv[2]}
 
-    data = urllib.parse.urlencode(value)
-    data = data.encode('ascii')
-    req = urllib.request.Request(url, data)
-    with urllib.request.urlopen(req) as res:
+    with requests.post(argv[1], data=values) as res:
         print(res.read().decode('utf-8'))
