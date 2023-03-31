fe5f38b452b5e923aa02f11c90fc3aa24154a24f
stanley <stanley6453@gmail.com>
2023-03-31 06:58:10 +0100

    created file 0x11-python-network_1/1-hbtn_header.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""fetches https://alx-intranet.hbtn.io/status"""
+"""fetches https://alx-intranet.hbtn.io/status."""
 
 import urllib.request
 response = urllib.request.urlopen("https://alx-intranet.hbtn.io/status").read()
