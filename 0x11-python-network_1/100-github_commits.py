4dcd75eeacf476ebcb7af4a4d4b1391d116ed7e9
stanley <stanley6453@gmail.com>
2023-03-31 16:07:06 +0100

    error fix

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -7,8 +7,22 @@ https://developer.github.com/v3/repos/commits/
 Print all commits by: `<sha>: <author name>` (one by line)
 """
 
-import urllib.parse
-import urllib.request
+import requests
 from sys import argv
 if __name__ == "__main__":
+    headers = {
+        'Accept': 'application/vnd.github+json',
+        'X-GitHub-Api-Version': '2022-11-28'
+    }
+    with requests\
+        .get(f'https://api.github.com/repos/{argv[2]}/{argv[1]}/commits',
+             headers=headers) as res:
+        list = res.json()
 
+        list.sort(reverse=True,
+                  key=lambda dict1: dict1['commit']['author']['date'])
+
+        for elem in list:
+            print(
+                f"{elem['sha']} \
+{elem['commit']['author']['name']}")
