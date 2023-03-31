4e427bb17dc0aeadca0b5fb7a33533d018ff48ed
stanley <stanley6453@gmail.com>
2023-03-31 14:38:38 +0100

    error fix

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -7,17 +7,17 @@ http://0.0.0.0:5000/search_user with the letter as a parameter.
 import requests
 from sys import argv
 if __name__ == "__main__":
-    url = 'http://4f0e93769549.a6cecdaa.alx-cod.online:5000/search_user'
+    url = 'http://0.0.0.0:5000/search_user'
     values = {'q': ''}
     if len(argv) > 1:
         values = {'q': argv[1]}
 
     with requests.post(url, data=values) as res:
         try:
-            obj = res.json()
-            if len(obj) == 0:
+            dict1 = res.json()
+            if len(dict1) == 0:
                 print("No result")
             else:
-                print(f'[{obj["id"]}] {obj["name"]}')
+                print(f'[{dict1["id"]}] {dict1["name"]}')
         except requests.exceptions.JSONDecodeError:
             print('Not a valid JSON')
