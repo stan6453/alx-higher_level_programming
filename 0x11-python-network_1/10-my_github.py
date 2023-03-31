d0379c26852c10da97831d90ea6656dfcdd7497e
stanley <stanley6453@gmail.com>
2023-03-31 15:12:22 +0100

    error fix

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -7,6 +7,10 @@ and uses the GitHub API to display your id
 import requests
 from sys import argv
 if __name__ == "__main__":
-    with requests.post('https://api.github.com/user',
-                       headers={'Authorization': f"Bearer {argv[2]}"}) as res:
-        print(res.json().get('id'))
+    try:
+        with requests.post('https://api.github.com/user',
+                        headers={'Authorization': f"Bearer {argv[2]}"}) as res:
+            print(res.json().get('id'))
+    except requests.exceptions.ConnectionError:
+        pass
+
