ea015c50c0006632bfb3aa0c8f54594de4813900
stanley <stanley6453@gmail.com>
2023-03-31 07:54:14 +0100

    creted file 0x11-python-network_1/4-hbtn_status.py

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""fetch https://alx-intranet.hbtn.io/status"""
+
+import urllib.request
+with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as res:
+    print('Body response:')
+    print(f"\t- type: {type(res.read().decode('utf-8'))}")
+    print(f"\t- content: {res.read().decode('utf-8')}")
