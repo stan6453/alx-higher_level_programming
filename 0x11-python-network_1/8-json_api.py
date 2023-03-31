7c9b16d1023c71a49f8b219170c301b2e3476cdd
stanley <stanley6453@gmail.com>
2023-03-31 15:06:02 +0100

    error fix

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -13,9 +13,6 @@ if __name__ == "__main__":
         values = {'q': argv[1][0]}
 
     with requests.post(url, data=values) as res:
-        if len(res.text) < 1:
-            print("No result")
-            exit(1)
         try:
             dict1 = res.json()
             if len(dict1) == 0:
